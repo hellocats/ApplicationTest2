f5a11fe88ecd0a93beb6fd78eb1fbfbee60ba410
hellocats <280922465@qq.com>
2016-11-25 19:02:44 +0800

    用户类

---
+++ app/src/main/java/com/example/administrator/applicationtest2/User.java
@@ -0,0 +1,36 @@
+package com.example.administrator.applicationtest2;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by Administrator on 2016-11-25.
+ */
+public class User extends BmobObject {
+    private int id;
+    private String name;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
