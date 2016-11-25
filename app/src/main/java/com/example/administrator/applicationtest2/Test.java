8f4cf2e173541c526ccb3035c8381588c0c5d72c
hellocats <280922465@qq.com>
2016-11-25 18:45:34 +0800

    新建测试文件

---
+++ app/src/main/java/com/example/administrator/applicationtest2/Test.java
@@ -0,0 +1,15 @@
+package com.example.administrator.applicationtest2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by he on 2016-11-25.
+ */
+public class Test extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
