3b758b51c48e6e284944e454c71ed0885aa2812f
dell <280922465@qq.com>
2016-12-15 23:38:56 +0800

    自定义view显示
    自定义view显示问题解决

--- app/src/main/java/com/example/administrator/applicationtest2/mvp/view/test/userDefine/MyTopbar.java
+++ app/src/main/java/com/example/administrator/applicationtest2/mvp/view/test/userDefine/MyTopbar.java
@@ -1,13 +1,12 @@
 package com.example.administrator.applicationtest2.mvp.view.test.userDefine;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -36,57 +35,114 @@ public class MyTopbar extends RelativeLayout {
 
     private LayoutParams leftParams,rightParams,titleParams;
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private topbarClickListener listener;
+
+    public interface topbarClickListener{
+        void leftClick();
+        void rightClick();
+    }
+    public void setOnTopbarClickListener(topbarClickListener listener){
+        this.listener = listener;
+    }
+
     public MyTopbar(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.Toolbar);
-        leftTextColor= ta.getColor(R.styleable.MyTopBar_myLeftTextColor,0);
-        leftBackground=ta.getDrawable(R.styleable.MyTopBar_myLeftBackground);
-        leftText = ta.getString(R.styleable.MyTopBar_myLeftText);
 
-        rightTextColor= ta.getColor(R.styleable.MyTopBar_myRightTextColor,0);
-        rightBackground=ta.getDrawable(R.styleable.MyTopBar_myRightBackground);
-        rightText = ta.getString(R.styleable.MyTopBar_myRightText);
-        
-        titleTextSize = ta.getDimension(R.styleable.MyTopBar_myTitleTextSize,0);
-        titleTextColor=ta.getColor(R.styleable.MyTopBar_myTitleTextColor,0);
-        title=ta.getString(R.styleable.MyTopBar_myTitleText);
+        getAttr(context, attrs);
+        setAttr(context);
+        addAllView();
+        setGlobalAttr();
+        setEvent();
+    }
 
+
+
+    /**
+     * 设置全局属性
+     */
+    private void setGlobalAttr() {
+        setBackgroundColor(Color.parseColor("#73DC7E"));
+    }
+    /**
+     * 获取自定义属性
+     * @param context
+     * @param attrs
+     */
+    private void getAttr(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MyTopbar);
+        leftTextColor= ta.getColor(R.styleable.MyTopbar_myLeftTextColor,0);
+        leftBackground=ta.getDrawable(R.styleable.MyTopbar_myLeftBackground);
+        leftText = ta.getString(R.styleable.MyTopbar_myLeftText);
+
+        rightTextColor= ta.getColor(R.styleable.MyTopbar_myRightTextColor,0);
+        rightBackground=ta.getDrawable(R.styleable.MyTopbar_myRightBackground);
+        rightText = ta.getString(R.styleable.MyTopbar_myRightText);
+
+        titleTextSize = ta.getDimension(R.styleable.MyTopbar_myTitleTextSize,16);
+        titleTextColor=ta.getColor(R.styleable.MyTopbar_myTitleTextColor,0);
+        title=ta.getString(R.styleable.MyTopbar_myTitleText);
         ta.recycle();
+    }
 
+    /**
+     * 设置自定义属性相应的值
+     * @param context
+     */
+    private void setAttr(Context context) {
         leftButton = new Button(context);
         rightButton = new Button(context);
         tvTitle = new TextView(context);
 
         leftButton.setTextColor(leftTextColor);
         leftButton.setBackground(leftBackground);
+        leftButton.setPadding(20, 0, 0, 0);
         leftButton.setText(leftText);
-        leftButton.setLines(2);
 
         rightButton.setTextColor(rightTextColor);
         rightButton.setBackground(rightBackground);
+        rightButton.setPadding(0, 0, 20, 0);
         rightButton.setText(rightText);
-        rightButton.setLines(2);
 
         tvTitle.setTextSize(titleTextSize);
         tvTitle.setTextColor(titleTextColor);
         tvTitle.setText(title);
         tvTitle.setGravity(Gravity.CENTER);
-        tvTitle.setLines(2);
+    }
 
-        leftParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT,TRUE);
+    /**
+     * 添加所有view
+     */
+    private void addAllView() {
+        leftParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);
         addView(leftButton,leftParams);
 
-        rightParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,TRUE);
+        rightParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+        rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
         addView(rightButton,rightParams);
 
         titleParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         titleParams.addRule(RelativeLayout.CENTER_IN_PARENT,TRUE);
         addView(tvTitle,titleParams);
-
-        setBackgroundColor(Color.parseColor("#73DC7E"));
-
     }
+
+    /**
+     * 添加事件
+     */
+    private void setEvent() {
+        leftButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.leftClick();
+            }
+        });
+        rightButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.rightClick();
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/administrator/applicationtest2/mvp/view/test/userDefine/UserDefineActivity.java
+++ app/src/main/java/com/example/administrator/applicationtest2/mvp/view/test/userDefine/UserDefineActivity.java
@@ -2,17 +2,33 @@ package com.example.administrator.applicationtest2.mvp.view.test.userDefine;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Button;
 
 import com.example.administrator.applicationtest2.R;
 import com.example.administrator.applicationtest2.util.baseClass.BaseClsActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by hepeng on 2016/12/14.
  */
 public class UserDefineActivity extends BaseClsActivity {
+    @BindView(R.id.test_userdefine_myTopBar)
+    MyTopbar myTopBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_userdefine_main);
+        myTopBar.setOnTopbarClickListener(new MyTopbar.topbarClickListener() {
+            @Override
+            public void leftClick() {
+                showToast("左边点击");
+            }
+
+            @Override
+            public void rightClick() {
+                showToast("右边点击");
+            }
+        });
     }
 }
